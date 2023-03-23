fixed a bug on the scaling factor and made working with the gripper

ILoSA.py
@@ -83,13 +83,17 @@ class ILoSA(Panda):
                 self.training_delta=np.zeros((1,3))
                 self.training_dK=np.zeros((1,3))
                 self.training_ori=np.zeros((1,4))
+                self.training_gripper=np.zeros((1,1))
                 
                 self.recorded_traj=self.recorded_traj.transpose()
                 self.recorded_ori=self.recorded_ori.transpose()
                 self.recorded_joint=self.recorded_joint.transpose()
+                self.recorded_gripper=self.recorded_gripper.transpose()
 
                 self.training_traj=np.concatenate((self.training_traj,self.recorded_traj ), axis=0)
                 self.training_ori=np.concatenate((self.training_ori,self.recorded_ori), axis=0)
+                self.training_gripper=np.concatenate((self.training_gripper,self.recorded_gripper), axis=0)
+
                 delta_x=resample(self.recorded_traj, step=2)
                 self.training_delta=np.concatenate((self.training_delta,delta_x), axis=0)
 
@@ -100,10 +104,18 @@ class ILoSA(Panda):
                 self.training_ori=np.delete(self.training_ori, 0,axis=0)
                 self.training_delta=np.delete(self.training_delta,0,axis=0)
                 self.training_dK=np.delete(self.training_dK,0,axis=0)
+                self.training_gripper=np.delete(self.training_gripper,0, axis=0)
 
             else:
+                self.recorded_traj=self.recorded_traj.transpose()
+                self.recorded_ori=self.recorded_ori.transpose()
+                self.recorded_joint=self.recorded_joint.transpose()
+                self.recorded_gripper=self.recorded_gripper.transpose()
+                
                 self.training_traj=np.concatenate((self.training_traj,self.recorded_traj ), axis=0)
                 self.training_ori=np.concatenate((self.training_ori,self.recorded_ori), axis=0)
+                self.training_gripper=np.concatenate((self.training_gripper,self.recorded_gripper), axis=0)
+
                 delta_x=resample(self.recorded_traj, step=2)
                 self.training_delta=np.concatenate((self.training_delta,delta_x), axis=0)
                 self.training_dK=np.concatenate((self.training_dK,np.zeros(np.shape(self.recorded_traj))), axis=0)
@@ -124,22 +136,28 @@ class ILoSA(Panda):
         training_traj=self.training_traj,
         training_ori = self.training_ori,
         training_delta=self.training_delta,
-        training_dK=self.training_dK) 
+        training_gripper=self.training_gripper,
+        training_dK=self.training_dK)
+        print(np.shape(self.training_ori))
+        print(np.shape(self.training_traj))  
 
     def load(self, file='last'):
         data =np.load(str(pathlib.Path().resolve())+'/data/'+str(file)+'.npz')
 
-        self.nullspace_traj=data['nullspace_traj'], 
-        self.nullspace_joints=data['nullspace_joints'], 
-        self.training_traj=data['training_traj'],
+        self.nullspace_traj=data['nullspace_traj']
+        self.nullspace_joints=data['nullspace_joints']
+        self.training_traj=data['training_traj']
         self.training_ori=data['training_ori']
-        self.training_delta=data['training_delta'],
-        self.training_dK=data['training_dK'] 
-        self.nullspace_traj=self.nullspace_traj[0]
-        self.nullspace_joints=  self.nullspace_joints[0]
-        self.training_traj=self.training_traj[0]
-        self.training_ori=self.training_ori[0]
-        self.training_delta=self.training_delta[0]
+        self.training_delta=data['training_delta']
+        self.training_dK=data['training_dK']
+        self.training_gripper=data['training_gripper'] 
+        self.nullspace_traj=self.nullspace_traj
+        self.nullspace_joints=  self.nullspace_joints
+        self.training_traj=self.training_traj
+        # print(np.shape(self.training_traj))
+        self.training_ori=self.training_ori
+        # print(np.shape(self.training_ori))
+        self.training_delta=self.training_delta
         self.training_dK=self.training_dK
 
     def Train_GPs(self):
@@ -247,20 +265,24 @@ class ILoSA(Panda):
             
             self.delta, self.K_tot = Force2Impedance(self.delta, self.K_tot, f_stable, self.attractor_lim)
             self.K_tot=[self.K_tot]
+            K_ori_scaling=self.K_ori
             self.scaling_factor = (1- self.sigma / self.Delta.max_var) / (1 - self.theta_stiffness)
-            if self.sigma / self.Stiffness.max_var > self.theta_stiffness: 
+            if self.sigma / self.Delta.max_var > self.theta_stiffness: 
                 self.K_tot=self.K_tot*self.scaling_factor
+                K_ori_scaling= self.K_ori*self.scaling_factor
             x_new = cart_pos[0][0] + self.delta[0]  
             y_new = cart_pos[0][1] + self.delta[1]  
             z_new = cart_pos[0][2] + self.delta[2]  
 
             quat_goal=self.training_ori[index_max_k_star,:]
-            
-            quat_goal=slerp_sat(self.cart_ori, quat_goal, 0.2)
+            gripper_goal=self.training_gripper[index_max_k_star,0]
+
+            quat_goal=slerp_sat(self.cart_ori, quat_goal, 0.1)
 
             pos_goal=[x_new, y_new, z_new]
             self.set_attractor(pos_goal,quat_goal)
-
+            self.move_gripper(gripper_goal)
+            #self.grasp_gripper(gripper_goal)
             null_stiff = [0]
 
             if self.NullSpaceControl:
@@ -275,7 +297,7 @@ class ILoSA(Panda):
                 null_stiff = [self.null_stiff]
             
             pos_stiff = [self.K_tot[0][0],self.K_tot[0][1],self.K_tot[0][2]]
-            rot_stiff = [self.K_ori, self.K_ori, self.K_ori]
+            rot_stiff = [K_ori_scaling,K_ori_scaling,K_ori_scaling]#[self.K_ori, self.K_ori, self.K_ori]
             self.set_stiffness(pos_stiff, rot_stiff, null_stiff)
             if verboose :
                 print("Delta")

panda.py
@@ -13,6 +13,7 @@ import pandas as pd
 import quaternion
 from sensor_msgs.msg import JointState, Joy
 from geometry_msgs.msg import Point, WrenchStamped, PoseStamped, Vector3
+from franka_gripper.msg import GraspActionGoal, HomingActionGoal, StopActionGoal, MoveActionGoal
 from std_msgs.msg import Float32MultiArray
 from sys import exit
 
@@ -27,7 +28,18 @@ class Panda():
 
         self.start = True
         self.end = False
-
+        
+        self.move_command=MoveActionGoal()
+        self.grasp_command = GraspActionGoal()
+        self.home_command = HomingActionGoal()
+        self.stop_command = StopActionGoal()
+        self.gripper_width = 0
+        self.move_command.goal.speed=1
+        self.grasp_command.goal.epsilon.inner = 0.1
+        self.grasp_command.goal.epsilon.outer = 0.1
+        self.grasp_command.goal.speed = 0.1
+        self.grasp_command.goal.force = 5
+        self.grasp_command.goal.width = 1
         # Start keyboard listener
         self.listener = Listener(on_press=self._on_press)
         self.listener.start()
@@ -48,7 +60,7 @@ class Panda():
 
     # gripper state subscriber
     def gripper_callback(self, data):
-        self.gripper_pos = np.array(data.position[7:9])
+        self.gripper_pos = data.position[7]+data.position[8]
 
 
     # spacemouse joystick subscriber
@@ -60,6 +72,25 @@ class Panda():
     def btns_callback(self, data):
         self.left_btn = data.buttons[0]
         self.right_btn = data.buttons[1]
+
+    def move_gripper(self,width):
+        self.move_command.goal.width=width
+        self.move_pub.publish(self.move_command)
+
+    def grasp_gripper(self, width):
+        if width < 0.07 and self.grasp_command.goal.width != 0:
+            self.grasp_command.goal.width = 0
+            self.grasp_pub.publish(self.grasp_command)
+
+        elif width > 0.07 and self.grasp_command.goal.width != 1:
+            self.grasp_command.goal.width = 1
+            self.grasp_pub.publish(self.grasp_command)
+
+    def home_gripper(self):
+        self.homing_pub.publish(self.home_command)
+
+    def stop_gripper(self):
+        self.stop_pub.publish(self.stop_command)    
 
     def connect_ROS(self):
         rospy.init_node('ILoSA', anonymous=True)
@@ -74,7 +105,15 @@ class Panda():
         self.goal_pub  = rospy.Publisher('/equilibrium_pose', PoseStamped, queue_size=0)
         self.stiff_pub = rospy.Publisher('/stiffness', Float32MultiArray, queue_size=0)
         self.configuration_pub = rospy.Publisher("/equilibrium_configuration",Float32MultiArray, queue_size=0)
-
+        self.grasp_pub = rospy.Publisher("/franka_gripper/grasp/goal", GraspActionGoal,
+                                           queue_size=0)
+        self.move_pub = rospy.Publisher("/franka_gripper/move/goal", MoveActionGoal,
+                                           queue_size=0)
+        self.homing_pub = rospy.Publisher("/franka_gripper/homing/goal", HomingActionGoal,
+                                          queue_size=0)
+        self.stop_pub = rospy.Publisher("/franka_gripper/stop/goal", StopActionGoal,
+                                          queue_size=0)
+        
     def set_stiffness(self,pos_stiff,rot_stiff,null_stiff):
         stiff_des = Float32MultiArray()
         stiff_des.data = np.array([pos_stiff[0], pos_stiff[1], pos_stiff[2], rot_stiff[0], rot_stiff[1], rot_stiff[2], null_stiff[0]]).astype(np.float32)
@@ -201,11 +240,14 @@ class Panda():
         self.recorded_traj = self.cart_pos
         self.recorded_ori  = self.cart_ori
         self.recorded_joint= self.joint_pos
+        self.recorded_gripper= self.gripper_pos
         while not self.end:
 
             self.recorded_traj = np.c_[self.recorded_traj, self.cart_pos]
             self.recorded_ori  = np.c_[self.recorded_ori,  self.cart_ori]
             self.recorded_joint = np.c_[self.recorded_joint, self.joint_pos]
+            self.recorded_gripper = np.c_[self.recorded_gripper, self.gripper_pos]
+
             r.sleep()
         
             