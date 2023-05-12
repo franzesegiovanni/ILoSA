8c4a9db870276e9dba4f3541de413888da8e1459
franzesegiovanni <giovanni.franzese94@gmail.com>
2023-05-12 18:05:56 +0200

    added missing indentation in main, created folder data and model

--- main.py
+++ main.py
@@ -14,46 +14,32 @@ if __name__ == '__main__':
     ILoSA.connect_ROS()
     time.sleep(5)
     ILoSA.home_gripper()
-#%% 
+    #%% 
     print("Recording of Nullspace contraints")
     ILoSA.Record_NullSpace()
-#%%     
+    #%%     
     time.sleep(1)
     print("Reset to the starting cartesian position")
     ILoSA.go_to_pose(ILoSA.nullspace_traj[:, 0])    
-#%%
+    #%%
     time.sleep(1)
     print("Record of the cartesian trajectory")
     ILoSA.Record_Demonstration()     
 
-#%%
+    #%%
     time.sleep(1)
     print("Save the data") 
     ILoSA.save()
-#%%
+    #%%
     time.sleep(1)
     print("Load the data") 
     ILoSA.load()    
-#%% 
+    #%% 
     time.sleep(1)
     print("Train the Gaussian Process Models")
     ILoSA.Train_GPs()
-#%%  
-    time.sleep(1)
-    print("Reset to the starting cartesian position")
-    start = PoseStamped()
-    ILoSA.home_gripper()
-    start.pose.position.x = ILoSA.recorded_traj[0,0]
-    start.pose.position.y = ILoSA.recorded_traj[0,1]
-    start.pose.position.z = ILoSA.recorded_traj[0,2]
-
-    start.pose.orientation.w = ILoSA.recorded_ori[0,0] 
-    start.pose.orientation.x = ILoSA.recorded_ori[0,1] 
-    start.pose.orientation.y = ILoSA.recorded_ori[0,2] 
-    start.pose.orientation.z = ILoSA.recorded_ori[0,3] 
-    ILoSA.move_gripper(ILoSA.recorded_gripper[0,0])
-    ILoSA.go_to_pose(start)
-#%%
+    ILoSA.save_models()
+    #%%
     time.sleep(1)
     print("Reset to the starting cartesian position")
     start = PoseStamped()
@@ -68,7 +54,7 @@ if __name__ == '__main__':
     start.pose.orientation.y = ILoSA.training_ori[0,2] 
     start.pose.orientation.z = ILoSA.training_ori[0,3] 
     ILoSA.go_to_pose(start)
-#%% 
+    #%% 
     time.sleep(1)
     print("Interactive Control Starting")
     ILoSA.Interactive_Control(verboose=False)
